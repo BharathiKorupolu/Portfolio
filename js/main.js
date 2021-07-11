da4007b95c4bf3a26975d7ccb4a779e77a57083b
Sai Bharathi Korupolu <saibharathi.k@infosys.com>
2021-07-11 17:06:00 +0530

    Second Commit

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 /*===== MENU SHOW =====*/ 
-const showMenu = (toggleId, navId) =>{
+const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
+        nav = document.getElementById(navId);
 
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
-            nav.classList.toggle('show')
-        })
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show');
+        });
     }
-}
-showMenu('nav-toggle','nav-menu')
+};
+showMenu('nav-toggle', 'nav-menu');
 
 /*===== ACTIVE AND REMOVE MENU =====*/
 const navLink = document.querySelectorAll('.nav__link');   
